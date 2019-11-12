763b2d69ccf5f0d1056f515c082fbd1725779104
Kamila <zhakupova.kamila@gmail.com>
2019-11-12 14:56:04 +0600

    Подключила проект к Sql Server

---
+++ ConnectToSqlServer/StartupForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ConnectToSqlServer
+{
+    partial class StartupForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(54, 75);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(173, 23);
+            this.connectButton.TabIndex = 0;
+            this.connectButton.Text = "Подключится к MyDB";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.ConnectButtonClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 161);
+            this.Controls.Add(this.connectButton);
+            this.Name = "Form1";
+            this.Text = "Подключится к Sql Server";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectButton;
+    }
+}
+
---
+++ ConnectToSqlServer/StartupForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ConnectToSqlServer
+{
+    public partial class StartupForm : Form
+    {
+        public StartupForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButtonClick(object sender, EventArgs eventArgs)
+        {
+            var builder = new SqlConnectionStringBuilder();
+            builder["Server"] = "DESKTOP-NVDGPN3";
+            builder["Database"] = "MyDB";
+            builder["Trusted_Connection"] = true;
+
+            using (var connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+                MessageBox.Show("Успешно подключено!");
+            }
+        }
+    }
+}
